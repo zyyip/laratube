b81473b4411821cfcec13aff24ed121b65b4b8fd
zyyip <64174391+zyyip@users.noreply.github.com>
2022-02-19 02:06:35 +0800

    Changed the default primary key to use a UUID instead of the normal increments

---
+++ app/Model.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model as BaseModel;
+
+class Model extends BaseModel
+{
+    public $incrementing = false;
+
+    protected static function boot(){
+        parent::boot();
+        static::creating(function ($model){
+            // getKeyName gets the primary key for the model
+            $model->{$model->getKeyName()} = (string) Str::uuid();
+        });
+    }
+}
