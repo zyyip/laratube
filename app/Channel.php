cac6c0ed0c7d972c03df87afcbfbeddd203463d1
zyyip <64174391+zyyip@users.noreply.github.com>
2022-02-20 01:06:24 +0800

    Finished part 8. To use spatie medialibrary to upload images and connect together with the channel's model

--- app/Channel.php
+++ app/Channel.php
@@ -2,15 +2,33 @@
 
 namespace App;
 
-class Channel extends Model
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\Models\Media;
+
+class Channel extends Model implements HasMedia
 {
     // protected $fillable = [
     //     'name'
     // ];
-
+    use HasMediaTrait;
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function image()
+    {
+        if($this->media->first()){
+            return $this->media->first()->getFullUrl('thumb');
+        }
+        return null;
+    }
+
+    public function registerMediaConversions(?Media $media = null)
+    {
+        $this->addMediaConversion('thumb')->width(100)->height(100);
     }
 }
